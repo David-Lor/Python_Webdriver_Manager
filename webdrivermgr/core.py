91fc8cf9ab329fc1517ea0c6014fdaeda15a3f76
EnforcerZhukov <davidl.eco@gmail.com>
2018-05-15 08:57:47 +0200

    Replaced cookies system with profile/data folder

--- webdrivermgr/core.py
+++ webdrivermgr/core.py
@@ -1,8 +1,7 @@
 
 #Native libraries
-import os
+#import os
 import atexit
-import pickle
 #Installed libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 
 #Constant Settings
 DRIVER_PATH = "chromedriver" #Path to chromedriver bin
+DATA_PATH = "ChromeDriverData" #Path to store browser data (history, cookies...)
 WINDOW_X = 1280 #Browser window size
 WINDOW_Y = 720
 TIMEOUT = 30 #Page Load Timeout
@@ -17,20 +17,18 @@ USERAGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gec
 
 class Webdriver(object):
     def __init__(self,
+        headless=True,
         driver_path=DRIVER_PATH,
+        data_path=DATA_PATH,
         winsize=(WINDOW_X, WINDOW_Y),
-        cookies_path=None,
-        autosave_cookies=True,
         user_agent=USERAGENT,
-        timeout=TIMEOUT,
-        headless=True
+        timeout=TIMEOUT
     ):
         """Initialize a Chrome Webdriver object using Selenium.
-        :param cookies_path: Path of cookies file (default=None=do not use cookies)
-        :param autosave_cookies: Autosave cookies at exit? (default=True)
         :param headless: Execute headless browser? (default=True)
         The following params are default to Constant Settings on the .py file:
         :param driver_path: Path of chromedriver executable
+        :param data_path: Path for browser data
         :param winsize: Browser window size, format: tuple/list (sizeX, sizeY)
         :param user_agent: User agent string for the browser
         :param timeout: Page load timeout
@@ -40,6 +38,7 @@ class Webdriver(object):
         if headless:
             opts.add_argument("--headless")
         #opts.add_argument("--window-size={},{}".format(winsize[0], winsize[1]))
+        opts.add_argument("user-data-dir="+data_path)
         opts.add_argument("user-agent="+user_agent)
         #Start browser
         self.browser = webdriver.Chrome(
@@ -50,16 +49,11 @@ class Webdriver(object):
         self.browser.set_page_load_timeout(timeout)
         #Some class attributes
         self.keys = Keys
-        self.cookies_path = cookies_path
-        self.autosave_cookies = autosave_cookies
         self.headless = headless
         self.window_size = winsize
-        #Load cookies
-        try:
-            self.load_cookies()
-        except FileNotFoundError: #If cookies file does not exist
-            self.save_cookies() #create it
-        #Atexit register (save cookies & stop browser when program ends)
+        self.data_path = data_path
+        self.user_agent = user_agent
+        #Atexit register (stop browser when program ends)
         @atexit.register
         def atexit_f():
             self.stop()
@@ -70,15 +64,10 @@ class Webdriver(object):
         """
         return self.browser
 
-    def stop(self, save_cookies=None):
-        """Stop the webdriver. For starting again, another Webdriver object must be initialized
-        :param save_cookies: Auto-save cookies before closing the browser?
-        If nothing is specified, value declared at autosave_cookies will be used
+    def stop(self):
+        """Stop the webdriver.
+        For starting again, another Webdriver object must be initialized
         """
-        if save_cookies is None:
-            save_cookies = self.autosave_cookies
-        if save_cookies:
-            self.save_cookies()
         self.browser.quit()
 
     def is_running(self):
@@ -106,24 +95,66 @@ class Webdriver(object):
         else:
             return sbytes
 
-    def save_cookies(self):
-        """Save the current cookies to a Pickle file
-        on the cookies_path provided when initializing the Webdriver.
+    def get_windows(self):
+        """Get all the windows/tabs currently opened on the browser
+        :return: List of windows/tabs names
         """
-        if self.cookies_path:
-            cookies = self.browser.get_cookies()
-            with open(self.cookies_path, "wb") as file:
-                pickle.dump(cookies, file)
+        return self.browser.window_handles
 
-    def load_cookies(self):
-        """Load the cookies saved on the Pickle file
-        on the cookies_path provided when initializing the Webdriver.
+    def switch_window(self, window=None, index=None):
+        """Switch to a window/tab of the browser.
+        Specify the window with:
+        :param window: Window name to switch to
+        :param index: Index of the window to switch to
         """
-        if self.cookies_path:
-            with open(self.cookies_path, "rb") as file:
-                cookies = pickle.load(file)
-                for c in cookies:
-                    try:
-                        self.browser.add_cookie(c)
-                    except:
-                        pass
+        w = None
+        if window is not None and index is None:
+            w = window
+        if window is None and index is not None:
+            w = self.get_windows()[index]
+        if w is not None:
+            self.browser.switch_to_window(w)
+
+    def close_window(self, window=None, index=None, switch=True):
+        """Close an opened window/tab of the browser.
+        Specify the window with:
+        :param window: Window name to switch to
+        :param index: Index of the window to switch to
+        Additional params:
+        :param switch: Index of the window to switch to after the operation
+        if switch=True (default), browser will switch to last available window
+        """
+        w = None
+        if window is not None and index is None:
+            w = window
+        if window is None and index is not None:
+            w = self.get_windows()[index]
+        if w is not None:
+            self.browser.switch_to_window(w)
+            self.browser.close()
+            if switch is True:
+                w = self.get_windows()[-1]
+            else:
+                w = self.get_windows()[switch]
+            self.switch_window(window=w)
+
+    def close_windows(self, startIndex, lastIndex=None):
+        """Close all opened windows/tabs on the browser between two indexes given (zero-indexed, both inclusive).
+        I.e. if we want to close all tabs except except the first one, startIndex=1.
+        After the operation, browser will switch to the last window available.
+        :param startIndex: index of first tab (required)
+        :param lastIndex: index of last tab (optional)
+        If lastIndex not provided, all tabs from startIndex will be closed
+        """
+        windows = self.get_windows()
+        if lastIndex is None:
+            lastIndex = len(windows) - 1
+        for i in range(startIndex, lastIndex+1):
+            try:
+                win = windows[i]
+                self.switch_window(window=win)
+                self.browser.close()
+            except:
+                pass
+        self.switch_window(index=-1)
+
